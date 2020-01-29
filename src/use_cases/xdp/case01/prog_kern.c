760c537ae0166ecab5a6ebf81c96e70897efd6da
davidcawork <davidcawork@gmail.com>
2020-01-29 11:10:40 +0100

    Fixed typos

--- src/use_cases/xdp/case01/prog_kern.c
+++ src/use_cases/xdp/case01/prog_kern.c
@@ -13,7 +13,7 @@
 #include "../common/xdp_stats_kern_user.h"
 #include "../common/xdp_stats_kern.h"
 	
-SEC("case01")
+SEC("xdp_case01")
 int xdp_pass_func(struct xdp_md *ctx)
 {
 	int action = XDP_PASS;
