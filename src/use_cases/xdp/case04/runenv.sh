33d018816a8ab7d8e4fea8f89891a57fd1e84d07
davidcawork <davidcawork@gmail.com>
2020-02-28 10:14:33 +0100

    Added the rest of runenv.sh scripts - case04-5

--- src/use_cases/xdp/case04/runenv.sh
+++ src/use_cases/xdp/case04/runenv.sh
@@ -29,32 +29,27 @@ function install {
 	printf 'Levantando escenario...\n' >&2
 		
 	check_prereq
-	
+
 	# Let's create netns
-	sudo ip netns add uno
-	sudo ip netns add dos
-	sudo ip netns add switch
+	ip netns add uno
+	ip netns add dos
+
 
 	# We've to create each interface and set into right netns
-	sudo ip link add uno type veth peer name veth0
-	sudo ip link set veth0 netns uno
-	sudo ip link set uno netns switch
-	sudo ip link add dos type veth peer name veth0
-	sudo ip link set veth0 netns dos
-	sudo ip link set dos netns switch
-	sudo ip link add switch type veth peer name gateway
-	sudo ip link set switch netns switch
-
+	ip link add uno type veth peer name veth0
+	ip link set veth0 netns uno
+	ip link add dos type veth peer name veth0
+	ip link set veth0 netns dos
+	
 	# Finally assign Ip's, and raise each one of them.
-	sudo ip link set gateway up
-	sudo ip addr add 10.0.0.1/24 dev gateway
-	sudo ip netns exec switch ip link set switch up
-	sudo ip netns exec switch ip link set uno up
-	sudo ip netns exec switch ip link set dos up
-	sudo ip netns exec uno ip link set veth0 up
-	sudo ip netns exec uno ip addr add 10.0.0.2/24 dev veth0
-	sudo ip netns exec dos ip link set veth0 up
-	sudo ip netns exec dos ip addr add 10.0.0.3/24 dev veth0
+	ip link set uno up
+	ip addr add 10.0.0.1/24 dev uno
+	ip link set dos up
+	ip addr add 10.0.0.3/24 dev dos
+	ip netns exec uno ip link set veth0 up
+	ip netns exec uno ip addr add 10.0.0.2/24 dev veth0
+	ip netns exec dos ip link set veth0 up
+	ip netns exec dos ip addr add 10.0.0.4/24 dev veth0
 }
 
 function clean {
