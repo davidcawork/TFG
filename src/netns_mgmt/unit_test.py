c7e98ab2c958bbb111d89bf4d6bdaf86c34649f9
davidcawork <davidcawork@gmail.com>
2020-04-18 22:57:08 +0200

    Added output unit test

--- src/netns_mgmt/unit_test.py
+++ src/netns_mgmt/unit_test.py
@@ -5,7 +5,7 @@ import subprocess, os, unittest, time
 class NamedNetns_test(unittest.TestCase):
 
     def setUp(self):
-        print("Setting up the scenario")
+        print("\n[Named Unittest] Setting up the scenario")
         if os.getuid() != 0:
             raise ValueError('Netns_mgmt must run as root.')
         
@@ -13,31 +13,31 @@ class NamedNetns_test(unittest.TestCase):
 	
 
     def test(self):
-        print("Running Netns_mgmt class")
+        print("[Named Unittest] Running Netns_mgmt class")
         with Netns_mgmt(nsname = 'test'):
             output = subprocess.check_output(['ip', 'a', 's'])
         self.assertEqual(output, subprocess.check_output(['ip', 'netns', 'exec', 'test', 'ip', 'a', 's']))
 
     def tearDown(self):
-        print("Teardown the scenario")
+        print("[Named Unittest] Teardown the scenario")
         os.system('ip netns del test')
 
 
 class UnnamedNetns_test(unittest.TestCase):
 
     def setUp(self):
-        print("Setting up the scenario")
+        print("\n[Unnamed Unittest] Setting up the scenario")
         self.process = subprocess.Popen(['unshare','-n','/bin/bash'], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 		
         
     def test(self):
-        print("Running Netns_mgmt class")
+        print("[Unnamed Unittest] Running Netns_mgmt class")
         with Netns_mgmt(nspid = self.process.pid):
             output = subprocess.check_output(['ip', 'a', 's'])
         self.assertEqual(output, subprocess.check_output(['nsenter', '-t', str(self.process.pid), '-n', 'ip', 'a', 's']))
     
     def tearDown(self):
-        print("Teardown the scenario")
+        print("[Unnamed Unittest] Teardown the scenario")
         self.process.kill()
         del(self.process)
 
