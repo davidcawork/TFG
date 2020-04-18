1e1dea4696b12234160a18cad14581ae4278f362
davidcawork <davidcawork@gmail.com>
2020-04-18 21:21:14 +0200

    Added unit test unamed network namespaces

--- src/netns_mgmt/netns_mgmt.py
+++ src/netns_mgmt/netns_mgmt.py
@@ -3,49 +3,54 @@
 import os
 from ctypes import cdll
 
-# http://man7.org/linux/man-pages/man2/setns.2.html
+# We make use of the setns system call from the libc library
+#
+# Check this out: http://man7.org/linux/man-pages/man2/setns.2.html
+# 
 libc = cdll.LoadLibrary('libc.so.6')
 setns_func = libc.setns
 
 
 class Netns_mgmt(object):
+   
+	"Class to manage the Network namespace in runtime"    
     
-    def __init__(self, nsname=None, nspath=None, nspid=None):
+
+	def __init__(self, nsname=None, nspath=None, nspid=None):
+		"The libc setns function is used to switch the current process between the different Netns."
+
+		self.current_path = Netns_mgmt.get_netns_path(netns_pid=os.getpid())
+		self.target_path = Netns_mgmt.get_netns_path(netns_path=nspath, netns_name=nsname, netns_pid=nspid)
+
+		if not self.target_path:
+			raise ValueError('invalid namespace')
 
 
-        self.current_path = Netns_mgmt.get_netns_path(netns_pid=os.getpid())
-        self.target_path = Netns_mgmt.get_netns_path(netns_path=nspath, netns_name=nsname, netns_pid=nspid)
+	def __enter__(self):
+		"We enter the netns, via name or via pid"
 
-        if not self.target_path:
-            raise ValueError('invalid namespace')
+		if os.getuid() != 0:
+			raise ValueError('Netns_mgmt must run as root.')
 
-
-
-    def __enter__(self):
-
-        if os.getuid() != 0:
-            raise ValueError('Netns_mgmt must run as root.')
-
-        self.current_ns = open(self.current_path)
-        with open(self.target_path) as fd:
-            setns_func(fd.fileno(), 0)
+		self.current_ns = open(self.current_path)
+		with open(self.target_path) as fd:
+			setns_func(fd.fileno(), 0)
 
 
+	def __exit__(self, *args):
+		"We left the netns, going back to the original netns"
 
-    def __exit__(self, *args):
+		setns_func(self.current_ns.fileno(), 0)
+		self.current_ns.close()
 
 
-        setns_func(self.current_ns.fileno(), 0)
-        self.current_ns.close()
+	@staticmethod 
+	def get_netns_path(netns_path=None, netns_name=None, netns_pid=None):
 
+		if netns_name:
+			netns_path = '/var/run/netns/' + str(netns_name)
+		elif netns_pid:
+			netns_path = '/proc/'+ str(netns_pid) +'/ns/net'
 
-    @staticmethod 
-    def get_netns_path(netns_path=None, netns_name=None, netns_pid=None):
-
-        if netns_name:
-            netns_path = '/var/run/netns/' + str(netns_name)
-        elif netns_pid:
-            netns_path = '/proc/'+ str(netns_pid) +'/ns/net'
-
-        return netns_path
+		return netns_path
 
--- src/netns_mgmt/unit_test.py
+++ src/netns_mgmt/unit_test.py
@@ -1,5 +1,6 @@
 from netns_mgmt import Netns_mgmt
-import subprocess, os, unittest
+from ctypes import *
+import subprocess, os, unittest, time
 
 
 class NamedNetns_test(unittest.TestCase):
@@ -22,6 +23,24 @@ class NamedNetns_test(unittest.TestCase):
         print("Teardown the scenario")
         os.system('ip netns del test')
 
+
+class UnnamedNetns_test(unittest.TestCase):
+
+    def setUp(self):
+        print("Setting up the scenario")
+        self.process = subprocess.Popen(['unshare','-n','/bin/bash'], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+		
+        
+    def test(self):
+        print("Running Netns_mgmt class")
+        with Netns_mgmt(nspid = self.process.pid):
+            output = subprocess.check_output(['ip', 'a', 's'])
+        self.assertEqual(output, subprocess.check_output(['nsenter', '-t', str(self.process.pid), '-n', 'ip', 'a', 's']))
+    
+    def tearDown(self):
+        print("Teardown the scenario")
+        self.process.kill()
+        del(self.process)
 
 if __name__ == '__main__':
     unittest.main() 
